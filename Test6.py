4442c5092f618f9738b5b3491e4cb409145a2e48
Tarun200025 <tarungupta2798@gmail.com>
2025-03-30 17:36:02 +0530

    Add files via upload

---
+++ Test6.py
@@ -0,0 +1,27 @@
+name = input('Enter the Name:')
+if name == 'tarun':
+        print(name,'The name is in our DB')
+elif name == 'vinay':
+        print(name,'The name is in our DB')
+elif name == 'Vishu':
+        print(name,'The name is in our DB')   
+elif name == 'Crow':
+        print(name,'The name is in our DB') 
+elif name == 'Black':
+        print(name,'The name is in our DB') 
+elif name == 'Pink':
+        print(name,'The name is in our DB')   
+elif name == 'Orange':
+        print(name,'The name is in our DB') 
+elif name == 'Virat':
+        print(name,'The name is in our DB')    
+elif name == 'Brown':
+        print(name,'The name is in our DB')
+elif name == 'good':
+        print(name,'The name is in our DB')
+else:
+        print(name,'The name is not in our DB')
+
+
+
+         
